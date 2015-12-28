345d68d7a13c9ada0b17d4ba0329e65a588bec54
Grace Han <hanpanpan200@gmail.com>
2015-12-28 15:27:36 +0800

    feat: init repo

---
+++ db/migrate/20151228020931_create_countries.rb
@@ -0,0 +1,9 @@
+class CreateCountries < ActiveRecord::Migration
+  def change
+    create_table :countries do |t|
+      t.string :name
+      t.boolean :enabled
+      t.timestamps null: false
+    end
+  end
+end
